ec35ae73290042e91d6d1bc98c4c00241799e0c5
SmokeyDot <107556982+SmokeyDot@users.noreply.github.com>
2024-10-16 19:38:00 +0530

    Update app.py

--- book/app.py
+++ book/app.py
@@ -4,37 +4,46 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the trained model and data files
-model = pickle.load(open('model.pkl', 'rb'))
-book_names = pickle.load(open('book_names.pkl', 'rb'))
-final_rating = pickle.load(open('final_rating.pkl', 'rb'))
-book_pivot = pickle.load(open('book_pivot.pkl', 'rb'))
+# Custom unpickler to handle '_unpickle_block' attribute
+class CustomUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'pandas._libs.internals' and name == '_unpickle_block':
+            from pandas.core.internals.blocks import _unpickle_block
+            return _unpickle_block
+        return super().find_class(module, name)
+
+# Load the trained model and data files using the custom unpickler
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('book_names.pkl', 'rb') as f:
+    book_names = pickle.load(f)
+
+with open('final_rating.pkl', 'rb') as f:
+    final_rating = CustomUnpickler(f).load()
+
+with open('book_pivot.pkl', 'rb') as f:
+    book_pivot = pickle.load(f)
 
 def fetch_poster(suggestion):
     book_name = []
     ids_index = []
     poster_url = []
-
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
-
-    for name in book_name[0]: 
+    for name in book_name[0]:
         ids = np.where(final_rating['title'] == name)[0][0]
         ids_index.append(ids)
-
     for idx in ids_index:
         url = final_rating.iloc[idx]['image_url']
         poster_url.append(url)
-
     return poster_url
 
 def recommend_book(book_name):
     books_list = []
     book_id = np.where(book_pivot.index == book_name)[0][0]
     distance, suggestion = model.kneighbors(book_pivot.iloc[book_id, :].values.reshape(1, -1), n_neighbors=6)
-
     poster_url = fetch_poster(suggestion)
-    
     for i in range(len(suggestion)):
         books = book_pivot.index[suggestion[i]]
         for j in books:
