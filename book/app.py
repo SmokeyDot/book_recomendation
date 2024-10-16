2dc4c8c161b1b4ff8a7ef6cdac4b93ff5bd79046
SmokeyDot <107556982+SmokeyDot@users.noreply.github.com>
2024-10-16 19:59:12 +0530

    Update app.py

--- book/app.py
+++ book/app.py
@@ -1,37 +1,40 @@
-import pandas as pd
 from flask import Flask, request, jsonify, render_template
-import numpy as np
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-# Load data from CSV instead of pickle
-final_rating = pd.read_csv('final_rating.csv')
-
-# Load the rest of your pickles
+# Load the trained model and data files
 model = pickle.load(open('model.pkl', 'rb'))
 book_names = pickle.load(open('book_names.pkl', 'rb'))
+final_rating = pickle.load(open('final_rating.pkl', 'rb'))
 book_pivot = pickle.load(open('book_pivot.pkl', 'rb'))
 
 def fetch_poster(suggestion):
     book_name = []
     ids_index = []
     poster_url = []
+
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
-    for name in book_name[0]:
+
+    for name in book_name[0]: 
         ids = np.where(final_rating['title'] == name)[0][0]
         ids_index.append(ids)
+
     for idx in ids_index:
         url = final_rating.iloc[idx]['image_url']
         poster_url.append(url)
+
     return poster_url
 
 def recommend_book(book_name):
     books_list = []
     book_id = np.where(book_pivot.index == book_name)[0][0]
     distance, suggestion = model.kneighbors(book_pivot.iloc[book_id, :].values.reshape(1, -1), n_neighbors=6)
+
     poster_url = fetch_poster(suggestion)
+    
     for i in range(len(suggestion)):
         books = book_pivot.index[suggestion[i]]
         for j in books:
